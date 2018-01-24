f6e5dc388e1144d537811493bdff008f95ba8bcb
Nyghor <dmateutudela@gmail.com>
2018-01-24 21:09:44 +0100

    [LCS-18] Añadido movimiento (WASD)

--- LCSEngine/ModuleCamera.cpp
+++ LCSEngine/ModuleCamera.cpp
@@ -1,7 +1,8 @@
+#include "Application.h"
 #include "ModuleCamera.h"
 #include "MathGeoLib/src/Math/float3x4.h"
 #include <glew.h>
-
+#include "ModuleInput.h"
 
 ModuleCamera::ModuleCamera() {}
 
@@ -11,24 +12,28 @@ bool ModuleCamera::init()
 {
 	frustum.verticalFov = 0.94248f;
 	frustum.horizontalFov = 1.41372f;
-	frustum.nearPlaneDistance = 0.1;
-	frustum.farPlaneDistance = 100;
-	float3 position = { 0, 0, 0 };
+	frustum.nearPlaneDistance = 0.1f;
+	frustum.farPlaneDistance = 100.f;
+	float3 position = { 0.f, 0.f, 0.f };
 	frustum.pos = position;
-	float3 front = { 0, 0, 1 };
+	float3 front = { 0.f, 0.f, -1.f };
 	frustum.front = front;
-	float3 up = { 0, 1, 0 };
+	float3 up = { 0.f, 1.f, 0.f };
 	frustum.up = up;
-	
 
 	return  true;
+}
+
+update_status ModuleCamera::update(const float deltaTime)
+{
+	moveCamera(deltaTime);
+	return UPDATE_CONTINUE;
 }
 
 bool ModuleCamera::cleanUp()
 {
 	return  true;
 }
-
 
 float* ModuleCamera::getViewMatrix()
 {
@@ -40,5 +45,45 @@ float* ModuleCamera::getProjectMatrix()
 	return frustum.ProjectionMatrix().Transposed().v[0];
 }
 
+void ModuleCamera::moveCamera(const float deltaTime)
+{
+	float speed = cameraSpeed*deltaTime;
+	float3 displacement = { 0.f, 0.f, 0.f };
 
+	if (App->input->getKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
+	{
+		speed *= 2.f;
+	}
 
+	if (App->input->getKey(SDL_SCANCODE_Q) == KEY_REPEAT)
+	{
+		displacement += { 0.f, 1.f, 0.f };
+	}
+
+	if (App->input->getKey(SDL_SCANCODE_E) == KEY_REPEAT)
+	{
+		displacement -= { 0.f, 1.f, 0.f };
+	}
+
+	if (App->input->getKey(SDL_SCANCODE_W) == KEY_REPEAT)
+	{
+		displacement += frustum.front;
+	}
+
+	if (App->input->getKey(SDL_SCANCODE_S) == KEY_REPEAT)
+	{
+		displacement -= frustum.front;
+	}
+
+	if (App->input->getKey(SDL_SCANCODE_D) == KEY_REPEAT)
+	{
+		displacement += frustum.WorldRight();
+	}
+
+	if (App->input->getKey(SDL_SCANCODE_A) == KEY_REPEAT)
+	{
+		displacement -= frustum.WorldRight();
+	}
+
+	frustum.Translate(displacement*speed);
+}
--- LCSEngine/ModuleCamera.h
+++ LCSEngine/ModuleCamera.h
@@ -14,19 +14,19 @@ public:
 	~ModuleCamera();
 
 	bool init();
+	update_status update(const float deltaTime);
 	bool cleanUp();
 	float* getViewMatrix();
 	float* getProjectMatrix();
 
 public:
-
 	Frustum frustum;
 
 private:
+	void moveCamera(const float deltaTime);
 
 private:
-
-
+	float cameraSpeed = 2.f;
 };
 
 #endif // __MODULECAMERA_H__
