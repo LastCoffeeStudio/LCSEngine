0e237a43f12c2f26fd1c1d6898a6d1932bbe3be3
carlos2380 <carlos.2380@hotmail.com>
2018-02-04 14:19:57 +0100

    Transform matrix created based on GUI.

--- LCSEngine/TransformComponent.cpp
+++ LCSEngine/TransformComponent.cpp
@@ -7,9 +7,18 @@ using namespace std;
 TransformComponent::TransformComponent(GameObject* gameObject, bool isEnable, bool isUnique) : Component(gameObject, isEnable, isUnique)
 {
 	typeComponent = TRANSFORM;
+	transform.SetIdentity();
+	matrixRotate.SetIdentity();
+	matrixScale.SetIdentity();
+	matrixTranslate.SetIdentity();
 }
 
 TransformComponent::~TransformComponent() { }
+
+void TransformComponent::updateTransform()
+{
+	transform = matrixRotate*matrixScale*matrixTranslate;
+}
 
 void TransformComponent::drawGUI()
 {
@@ -18,20 +27,27 @@ void TransformComponent::drawGUI()
 
 		if (ImGui::SliderFloat3("Position", &position[0], -100.f/*numeric_limits<float>::min()*/, 100.f/*numeric_limits<float>::max()*/))
 		{
-			//glLightModelfv(GL_LIGHT_MODEL_AMBIENT, alColor);
+			matrixTranslate.SetIdentity();
+			matrixTranslate[0][3] = position[0];
+			matrixTranslate[1][3] = position[1];
+			matrixTranslate[2][3] = position[2];
+			updateTransform();
+			//matrixTranslate.SetIdentity();
 		}
 
-		if (ImGui::SliderFloat3("Sclale", &scale[0], -100.f/*numeric_limits<float>::min()*/, 100.f/*numeric_limits<float>::max()*/))
+		if (ImGui::SliderFloat3("Sclale", &scale[0], 0.f/*numeric_limits<float>::min()*/, 100.f/*numeric_limits<float>::max()*/))
 		{
-			//glLightModelfv(GL_LIGHT_MODEL_AMBIENT, alColor);
+			matrixScale.SetIdentity();
+			matrixScale[0][0] = position[0];
+			matrixScale[1][1] = position[1];
+			matrixScale[2][2] = position[2];
+			updateTransform();
 		}
 
 		if (ImGui::SliderAngle3("Rotation", &rotation[0]))
 		{
 			rotationQad = Quat::FromEulerXYZ(rotation.x, rotation.y, rotation.z);
+			matrixRotate = rotationQad.ToFloat4x4();
 		}
 	}
-	
-	
-
 }
