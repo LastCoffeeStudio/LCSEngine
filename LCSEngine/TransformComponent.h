c22960175f58dc08a25d294182aaf2f6b9a0b7ca
carlos2380 <carlos.2380@hotmail.com>
2018-02-04 14:21:24 +0100

    isEnable true in default

--- LCSEngine/TransformComponent.h
+++ LCSEngine/TransformComponent.h
@@ -7,7 +7,7 @@
 class TransformComponent : public Component
 {
 public:
-	TransformComponent(GameObject* gameObject, bool isEnable = false, bool isUnique = true);
+	TransformComponent(GameObject* gameObject, bool isEnable = true, bool isUnique = true);
 	~TransformComponent();
 
 	void drawGUI() override;
