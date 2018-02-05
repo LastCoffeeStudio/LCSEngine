a6a2301b43a21186a9bf686cf7afab5108363b3a
Casa\Adrián Meizoso <trucuta5@hotmail.com>
2018-02-05 23:39:09 +0100

    LCS-52 - Resolved conflict

--- LCSEngine/GameObject.h
+++ LCSEngine/GameObject.h
@@ -1,10 +1,7 @@
 #ifndef __GAMEOBJECT_H__
 #define __GAMEOBJECT_H__
 
-<<<<<<< HEAD
-=======
 #include "glew.h"
->>>>>>> c6441b0d1afd99ed34576c2062ef822570ecb2c4
 #include <vector>
 using namespace std;
 
@@ -31,13 +28,11 @@ public:
 	vector<Component*> components;
 	vector<GameObject*> children;
 	GameObject* parent = nullptr;
-<<<<<<< HEAD
 	int nameNumber = 0;
 
 private:
 	string getFinalName(string name);
 	string initialName;
-=======
 
 	/*Create map to know if it has a component (updated when component added or deleted)*/
 
@@ -45,7 +40,6 @@ private:
 	vector<float> verticesVBO;
 	GLuint idVertVBO = 0;
 	float lengthX, lengthY, lengthZ;
->>>>>>> c6441b0d1afd99ed34576c2062ef822570ecb2c4
 };
 
 #endif // __GAMEOBJECT_H__
