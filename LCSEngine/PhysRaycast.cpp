009adfd39f6bc7c352bdae69870609110a5def46
Nyghor <dmateutudela@gmail.com>
2018-02-14 20:06:13 +0100

    [LCS-84] Now raycast only checks gameobjects whose aabb collides with it

--- LCSEngine/PhysRaycast.cpp
+++ LCSEngine/PhysRaycast.cpp
@@ -32,24 +32,27 @@ bool PhysRaycast::castRay(float3 origin, float3 direction, float distance)
 		{
 			if ((*it)->typeComponent == MESH)
 			{
-				MeshComponent* mesh = (MeshComponent*)(*it);
-				Triangle t;
 				LineSegment l = LineSegment(origin, origin + (direction * distance));
-				l.Transform(node->id.Transposed().Inverted());
-
-				for (unsigned int i = 0; i < mesh->verticesVBO.size(); i += 3)
+				if (l.Intersects(node->obb.MinimalEnclosingAABB()))
 				{
-					t.a = mesh->verticesVBO[i];
-					t.b = mesh->verticesVBO[i + 1];
-					t.c = mesh->verticesVBO[i + 2];
+					MeshComponent* mesh = (MeshComponent*)(*it);
+					Triangle t;
+					l.Transform(node->id.Transposed().Inverted());
 
-					float dist;
-					float3 intersectionPoint;
-					if (l.Intersects(t, &dist, &intersectionPoint) && dist < minDist)
+					for (unsigned int i = 0; i < mesh->verticesVBO.size(); i += 3)
 					{
-						minDist = dist;
-						gameObject = node;
-						hits = true;
+						t.a = mesh->verticesVBO[i];
+						t.b = mesh->verticesVBO[i + 1];
+						t.c = mesh->verticesVBO[i + 2];
+
+						float dist;
+						float3 intersectionPoint;
+						if (l.Intersects(t, &dist, &intersectionPoint) && dist < minDist)
+						{
+							minDist = dist;
+							gameObject = node;
+							hits = true;
+						}
 					}
 				}
 			}
