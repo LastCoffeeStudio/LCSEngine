2c5937415154303a1baa0b97a1a0239e94fad65e
carlos2380 <carlos.2380@hotmail.com>
2018-03-04 13:54:22 +0100

    [LCS-117] Try creation moduleAudio Wwise.

--- LCSEngine/ModuleAudio.h
+++ LCSEngine/ModuleAudio.h
@@ -6,9 +6,6 @@
 
 #define DEFAULT_MUSIC_FADE_TIME 2.0f
 
-struct _Mix_Music;
-struct Mix_Chunk;
-typedef struct _Mix_Music Mix_Music;
 
 class ModuleAudio : public Module
 {
@@ -17,22 +14,12 @@ public:
 	ModuleAudio(bool start_enabled = true);
 	~ModuleAudio();
 
-	bool init();
+	bool initSoundEngine();
 	bool cleanUp();
 
-	// Play a music file
-	bool playMusic(const char* path, float fade_time = DEFAULT_MUSIC_FADE_TIME);
-
-	// Load a WAV in memory
-	unsigned int loadFx(const char* path);
-
-	// Play a previously loaded WAV
-	bool playFx(unsigned int fx, int repeat = 0);
+	
 
 private:
-
-	Mix_Music*	music = nullptr;
-	std::vector<Mix_Chunk*>	fx;
 };
 
 #endif // __MODULEAUDIO_H__
